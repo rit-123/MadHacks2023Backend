116945795bdf19d99edbffb11465a32cddbd2502
Charger1256A <aditya.ram112005@gmail.com>
2023-11-11 17:39:38 -0600

    tested fetch

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import os
 
 from flask import Flask, flash, redirect, render_template, request, session, url_for
-from flask_session import Session
+from flask_session import *
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 
+
+
 # Configure application
 app = Flask(__name__)
+
+
 
 @app.after_request
 def after_request(response):
@@ -17,21 +21,23 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-@app_route("/login", methods=["GET","POST"])
+@app.route("/login", methods=["GET","POST"])
 def login():
-    
-    session.clear()
 
+    return "rese", 600
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
-        confirmpassword = request.form.get("confirmpassword")
+        # confirmpassword = request.form.get("confirmpassword")
 
         # Make sure username is not empty
         if not username:
-            return "Error: Username cannot be emnpty", 400
+            return json.dumps({"err":"ranodm err"}), 400
         # Make sure username doesn't already exist
 
         # Make sure passwords and confirm password match
 
         # add user and hashed password to the database
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, dubug=True)
